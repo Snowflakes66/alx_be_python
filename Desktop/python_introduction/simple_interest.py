0a6e499a6bbb87f20bff3e10b0e3dd1f9333a947
James Audu <ayomideyolasunkanmi25@gmail.com>
2024-12-17 18:10:30 -0800

    initial commit

---
+++ Desktop/python_introduction/simple_interest.py
@@ -0,0 +1,5 @@
+principal = 1000  # Principal amount ($1000)
+rate = 0.05  # Annual interest rate (5%)
+time = 3  # Time the money is invested for (3 years)
+interest = principal * rate * time
+print("The simple interest is", interest)
